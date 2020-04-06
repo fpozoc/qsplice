32fc2c6d9ea0b4872e40c060aab681b0aa052892
Fernando Pozo <fpozoc@cnio.es>
2020-04-06 13:16:27 +0200

    Initial commit

---
+++ intronator.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" generate_introns.py
+
+Usage: python intronator.py --gencode 27
+___
+--help | -h Display documentation
+
+DESCRIPTION 
+
+This file can also be imported as a module and contains the following functions:
+    * create_dir - returns the path of the directory created.
+    * generate_introns - returns the path of the introns file.
+
+TO DO:  
+    *
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import argparse, os, re, subprocess
+import source
+
+__author__ = "Fernando Pozo"
+__copyright__ = "Copyright 2020"
+__license__ = "GNU General Public License"
+__version__ = "1.0.1"
+__maintainer__ = "Fernando Pozo"
+__email__ = "fpozoc@cnio.es"
+__status__ = "Production"
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-g', '--gencode', type=int, help='GENCODE version selected.')
+    parser.add_argument('-f', '--file', type=str, help='Custom or not GENCODE gff file.')                        
+    args = parser.parse_args()
+
+    if args.gencode:
+        path = source.Gencode(version=args.gencode, specie='human').download(outdir='genomes', type='gff3')
+    else:
+        path = args.file
+
+    generate_introns(path, intronsdir='introns')
+
+
+def create_dir(dirpath: str) -> str: 
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+    return os.path.abspath(dirpath)
+
+
+def generate_introns(inpath: str, intronsdir: str = '.') -> str:
+    '''This function generates introns file from gff*.gz file path.
+
+    Args:
+       inpath (str):  The name of the annotation file to process.
+
+    Returns:
+        outpath (str): Path where file has been stored.
+    '''
+    subprocess.call('which gt || apt install gt', shell=True)
+    # subprocess.call('which rg || apt install rg', shell=True)
+
+    create_dir(intronsdir)
+    intronsname = os.path.basename(re.sub('.gff*.gz', '.introns.tsv', inpath) )
+    outpath = os.path.abspath(os.path.join(intronsdir, intronsname))
+
+    # cmd_awk = r"awk -F'[\t=]' '{print $1,$4,$5,$7,$10}' OFS='\t'"
+    # cmd_gt = f"zcat {inpath} | rg '(#|gene_type=protein_coding)' | gt gff3 -retainids -addintrons | rg 'intron' | {cmd_awk} > {outpath}"
+    cmd_gt = f"zcat {inpath} | gt gff3 -retainids -addintrons > {outpath}"
+    subprocess.call(cmd_gt, shell=True)
+
+    print(f'{outpath} has been generated.')
+    return outpath
+        
+
+if __name__ == "__main__":
+    main()
---
+++ source.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" source.py
+
+DESCRIPTION 
+
+This file can also be imported as a module and contains the following functions:
+    * Gencode
+
+TO DO:  
+    *
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import ftplib, os, re, subprocess
+from utils import create_dir
+
+class Gencode():
+    
+    def __init__(self, version: int, specie: str):
+        self.version = version
+        self.specie = specie
+        self.url = f'ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_{specie}/release_{version}'
+        self.ftpfiles = []
+        self.filepath = ''
+        
+    def connect_ftp(self):    # show last path of file
+        url_site = self.url.split('ftp://')[1].split('/')[0]
+        url_path = self.url.split(url_site)[1]
+        fc = ftplib.FTP(url_site)
+        try:
+            fc.login()
+            print(f'Conexion to {self.url} done.')
+        except RuntimeError:
+            print('Conexion cannot be established.')
+            return
+        fc.cwd(url_path)
+        self.ftpfiles = fc.nlst()
+    
+    def show_files(self):
+        print(self.ftpfiles)
+
+    def regexes(self) -> []:
+        catdict = {
+            'all': '.', 
+            'transcripts': r'gencode.v\d\d.pc_transcripts.fa.gz',
+            'translation': r'gencode.v\d\d.pc_translations.fa.gz',
+            'gff3': r'gencode.v\d\d.annotation.gff3.gz',
+            'gtf': r'gencode.v\d\d.annotation.gtf.gz',
+            'metadata': r'metadata'
+            }
+        return catdict
+
+    def download(self, outdir: str = '.', type: str = None, pattern: str = None):
+        self.connect_ftp()
+        create_dir(outdir)
+        catdict = self.regexes()
+        if type == None and pattern == None:
+            print(f'Please, select which file you want to download from {self.ftpfiles}')
+        filename = [p for p in self.ftpfiles if re.search(catdict[type] if type else pattern, p)]
+        urlname = os.path.join(self.url, filename[0])
+        try:
+            subprocess.call(f'wget -P {outdir} -nc --quiet {urlname}', shell=True)
+            filepath = os.path.join(outdir, os.path.basename(urlname))
+            print(f'{filepath} downloaded.')
+            return filepath
+        except subprocess.CalledProcessError:
+            print("Failed calling wget - Aborting")
+            return None
