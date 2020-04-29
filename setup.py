90130bf268e0443984cc24528cf10782e7be60df
fpozoc <fpozoc@cnio.es>
2020-04-29 19:04:25 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='QSplice',
+      version='1.0.0',
+      description='Quantifying splice junctions coverage from SJ.out.tab released by STAR mapping it to genome positions.',
+      author='Fernando Pozo',
+      author_email='fpozoc@cnio.es',
+      url='https://gitlab.com/fpozoc/qsplice.git',
+      download_url='https://gitlab.com/fpozoc/qsplice/tarball/0.1.0',
+      license='GNU General Public License',
+      install_requires=
+            ['pandas', 
+             'numpy'],
+      packages=find_packages()
+     )
